062010ce08cb135b3fea223f27b9c81ab9dd8da7
Yae-Htet-Aung <yaetactaung@gmail.com>
2023-12-20 14:35:17 +0630

    opacity done

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,10 +3,10 @@ import Logo from '../assets/resources/Logo.png'
 import Profile from '../assets/resources/profile.png'
 import Bell from '../assets/resources/bell.png'
 
-const Navigation = ({active}) => {
-  console.log('active? in navi >> ', active)  
+const Navigation = ({showModal, showAlert}) => {
+
   return (
-    <div className={`flex items-center justify-between w-full h-[82px] px-9 py-3 bg-[#54bab9] ${active? ' ' : 'opacity-70'} `}>
+    <div className={`flex items-center justify-between w-full h-[82px] px-9 py-3 bg-[#54bab9] ${ showModal || showAlert ? 'opacity-60 ' : ''} `}>
       <div>
         <img src={`${Logo}`} alt="" className='w-[50px] h-[60px]' />
       </div>
